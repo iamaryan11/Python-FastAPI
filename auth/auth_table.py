dc553a894bf2bc8a65afadcd5c04028c7fc6b7e5
AryanLuv <aryaneos11@gmail.com>
2026-02-09 19:53:06 +0530

    Add the auth table info needed for authentication and crucial user data

---
+++ auth/auth_table.py
@@ -0,0 +1,11 @@
+from auth_database import engine,Base
+
+# just : import model, sqlalchemy will automatically stored the data to metadata
+import models
+
+
+# the line means that : I am asking sqlalchemy to create all the tables that are mentioned in the model.py, Class is the table as per sqlalchemy
+Base.metadata.create_all(bind=engine)
+
+
+# run the file as: python create_table.py
